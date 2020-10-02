1c73b6c1c47df441621386c281e000b073056bb6
e-eight <soham@iastate.edu>
2020-10-02 03:54:36 -0500

    Added story generator and random integer generator.

---
+++ bank.py
@@ -0,0 +1,39 @@
+adjectives = {
+    # Personality adjectives
+    "pos_adj": [
+        "Gregarious",
+        "affable",
+        "bubbly",
+        "charming",
+        "charismatic",
+        "benevolent",
+        "sprightly",
+        "buoyant",
+        "brave",
+        "courageous",
+        "faithful",
+    ],
+    "neg_adj": [
+        "Conceited",
+        "exasperating",
+        "dishonorable",
+        "depraved",
+        "fiendish",
+        "diabolic",
+        "wicked",
+        "malicious",
+        "malevolent",
+        "serpentine",
+        "cynical",
+        "pompous",
+    ],
+    # Appearance/smell adjectives
+    "app_neg_adj": ["dank", "grimy", "sour"],
+    "app_pos_adj": ["beautiful", "pretty"],
+    "color": ["blue", "mauve", "olive", "burgundy"],
+}
+
+stories_db = [
+    "You ate a cursed USER_FRUIT and now you can't see the color QC_COLOR.",
+    "A QC_NEG_ADJ picksie stole your USER_COLOR USER_NOUN, and now you search high and low to recover it.",
+]
---
+++ story_generator.py
@@ -0,0 +1,31 @@
+import re
+
+from bank import adjectives
+from qrandom import QuantumRandomInt
+
+
+class StoryGenerator:
+    def __init__(self, stories):
+        self.stories = stories
+
+    def generate(self, backend=None):
+        story_chooser = QuantumRandomInt(0, len(self.stories) - 1)
+        idx = story_chooser.generate(1, backend)[0]
+        story = self.stories[idx]
+
+        user = re.compile("USER_\w+")
+        qc = re.compile("QC_\w+")
+
+        for user_category in user.findall(story):
+            category = user_category.partition("_")[2].lower()
+            word = str(input(f"Choose a {category}: "))
+            story = story.replace(user_category, word, 1)
+
+        for qc_category in qc.findall(story):
+            category = qc_category.partition("_")[2].lower()
+            word_chooser = QuantumRandomInt(0, len(adjectives[category]) - 1)
+            idx = word_chooser.generate(1, backend)[0]
+            word = adjectives[category][idx]
+            story = story.replace(qc_category, word, 1)
+
+        return story
